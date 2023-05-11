783d294d971d7b4fa79d0776f718f1babfc588c7
yujin <miru93198@gmail.com>
2023-05-11 16:27:26 +0900

    time & place 생성

--- src/components/MainPage/Calendar/AddContent.js
+++ src/components/MainPage/Calendar/AddContent.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import MiniCalendar from "./MiniCalendar";
 import ColorCom from "./ColorCom";
+import TestCal from "../TestCal";
+import { BiMinus, BiSearch } from "react-icons/bi";
+
 const AddTitle = styled.input`
   font-size: 24px;
   border: none;
@@ -12,13 +15,80 @@ const AddTitle = styled.input`
   //box-shadow: 3px 3px 10px #b3b3b3;
   width: 80%;
 `;
+const TitleFont = styled.div`
+  font-weight: 700;
+  font-size: 13px;
+  line-height: 16px;
+  color: #535571;
+  padding: 10px;
+  ${css`
+    &:after {
+      content: "  >";
+    }
+  `}
+`;
+const AddOther = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  width: 60%;
+`;
+const Right = styled.div`
+  width: 40%;
+`;
+const TimeDiv = styled.div`
+  display: flex;
+  width: 100%;
+`;
 
+const Time = styled.input`
+  width: 40%;
+  margin: 10px;
+`;
+
+const PlaceButton = styled.div`
+  width: 117px;
+  height: 23px;
+  background: #ffffff;
+  border: 0.5px solid #535571;
+  border-radius: 5px;
+  margin: 20px;
+  cursor: pointer;
+  display: flex;
+`;
+const PlaceText = styled.div`
+  font-size: 11px;
+  line-height: 13px;
+  margin: 5px;
+  color: #535571;
+`;
 const AddContent = () => {
   return (
     <>
       <AddTitle type="text" placeholder="일정 제목"></AddTitle>
-      <ColorCom />
-      <MiniCalendar />
+
+      <AddOther>
+        <Left>
+          <ColorCom />
+          <TitleFont>날짜</TitleFont>
+          <MiniCalendar className="MiniCalendar" />
+          {/* <TestCal /> */}
+        </Left>
+        <Right>
+          <TitleFont>시간</TitleFont>
+          <TimeDiv>
+            <Time type="time"></Time>
+            <BiMinus className="BiMinus" />
+            <Time type="time"></Time>
+          </TimeDiv>
+          <TitleFont>장소</TitleFont>
+          <PlaceButton>
+            <BiSearch className="BiSearch" />
+            <PlaceText>회의실 예약하기</PlaceText>
+          </PlaceButton>
+          <TitleFont>멤버</TitleFont>
+        </Right>
+      </AddOther>
     </>
   );
 };
--- src/data.js
+++ src/data.js
@@ -1,23 +1,23 @@
 const data = [
-  { id: 999, title: "event1", date: "2023-05-01", color: "#6F5CEA" },
-  { id: 999, title: "event2", date: "2023-05-09", color: "#76CAF8" },
+  { id: 999, title: "event1", date: "2023-05-01", color: "#548ff7" },
+  { id: 999, title: "event2", date: "2023-05-09", color: "#bedb84" },
   {
     title: "event3",
     start: "2023-05-12",
     end: "2023-05-14",
-    color: "#F5CB72",
+    color: "#9ad8dd",
   },
   {
     title: "박하민",
     start: "2023-05-16",
     end: "2023-05-19",
-    color: "red",
+    color: "ef888b",
   },
   {
     title: "박하민",
     start: "2023-05-16",
     end: "2023-05-19",
-    color: "red",
+    color: "ef888b",
   },
 ];
 
