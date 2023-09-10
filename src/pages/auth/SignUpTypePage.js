ef8354c8b4fb4cebf4677b5119a12f1da57f0294
mintmin <mintmin0320@gmail.com>
2023-09-11 04:33:13 +0900

    fix 대소문자 구분 활성화

---
+++ src/pages/auth/SignUpTypePage.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+
+import SignUp from "../../components/auth/SignUp";
+
+// icon, bg-color
+import color from "./../../assets/color.png";
+
+const Test = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #f5f3fe;
+`;
+const MainDiv = styled.div`
+  position: absolute;
+  width: 90vw;
+  height: 80vh;
+  margin-top: 78px;
+  margin-left: 69px;
+  margin-right: 69px;
+  background: #f8f8f8;
+  box-shadow: 10px 10px 30px rgba(0, 0, 0, 0.2);
+  border-radius: 20px;
+  display: flex;
+  flex-direction: row;
+  z-index: 2;
+`;
+const BackColor = styled.img`
+  position: absolute;
+  width: 548px;
+  height: 503px;
+  margin-left: 100px;
+  margin-top: 100px;
+  background: #f8f8f8;
+  z-index: 1;
+`;
+
+const SignUpTypePage = () => {
+  return (
+    <Test>
+      <MainDiv className="MainDiv">
+        <BackColor src={color} style={{ opacity: 0.2 }} />
+        <SignUp />
+      </MainDiv>
+    </Test>
+  );
+};
+
+export default SignUpTypePage;
