dc5a98ea1c87c6878082e235f546d9c6c6435e48
mintmin <mintmin0320@gmail.com>
2023-09-18 21:41:03 +0900

    refactor: 기업 회원가입 api 통신 로직 수정

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,24 +2,34 @@ import axios from 'axios';
 
 const BASE_URL = `https://${process.env.REACT_APP_SERVER_URI}`;
 
-export const fetchSignIn = async (data) => {
+export const fetchSignIn = (data) => {
   try {
-    const res = await axios.post(`${BASE_URL}/meeti/user/login`, data);
+    const res = axios.post(`${BASE_URL}/meeti/user/login`, data);
     const { accessToken, refreshToken } = res.data;
 
     localStorage.setItem('accessToken', accessToken);
     localStorage.setItem('refreshToken', refreshToken);
     localStorage.setItem('userId', res.data.id);
 
-    return res.data;
+    return res;
   } catch (error) {
     throw error;
   }
 };
 
-export const fetchSignUp = (data) => {
+export const fetchPersonalSignUp = (data) => {
   try {
     const res = axios.post(`${BASE_URL}/meeti/user/join`, data);
+
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchOfficeSignUp = (data) => {
+  try {
+    const res = axios.post(`${BASE_URL}/meeti/user/join/office`, data);
 
     return res;
   } catch (error) {
--- src/pages/auth/SignUpCorpPage.js
+++ src/pages/auth/SignUpCorpPage.js
@@ -3,186 +3,23 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import styled from "styled-components";
 
+// apis
+import { fetchOfficeSignUp } from '../../api/auth';
+
+// icons
 import { MdWork } from "react-icons/md";
 import { IoIosArrowForward } from "react-icons/io";
-import color from "./../../assets/color.png";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 
-const SignUpCorpPage = () => {
-  const navigate = useNavigate();
-  const [state, setState] = useState({
-    email: "",
-    userPw: "",
-    name: "",
-    authCode: "",
-    codeCheck: true, // 인증번호 Input 활성화
-    emailCheck: false, // 이메일 인증을 완료해야 회원가입 버튼이 보이게함
-  });
+import color from "./../../assets/color.png";
 
-  const _handleInputChange = e => {
-    setState({
-      ...state,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  // 인증코드 요청
-  const getAuthNum = async e => {
-    e.preventDefault();
-    const url = `http://${process.env.REACT_APP_SECRET_URL}/users/email-verify`;
-    const data = {
-      email: state.email,
-    };
-    try {
-      const res = await axios.post(url, data, { withCredentials: true });
-      setState({
-        ...state,
-        codeCheck: false,
-      });
-      console.log(res);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // 인증코드 확인
-  const codeCheck = async e => {
-    e.preventDefault();
-    const url = `http://${process.env.REACT_APP_SERVER_URI}/users/email-authentication`;
-    const data = {
-      authNum: state.authCode,
-    };
-    try {
-      const res = await axios.post(url, data);
-      console.log(res);
-      if (res.data.result) {
-        setState({
-          ...state,
-          emailCheck: true,
-        });
-        alert("인증 성공");
-      } else {
-        alert("잘못된 인증번호입니다.");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // 회원가입
-  const handleSubmit = async e => {
-    e.preventDefault();
-    const url = `https://${process.env.REACT_APP_SERVER_URI}/users/sign-up`;
-    const data = {
-      email: state.email,
-      name: state.name,
-      password: state.password,
-    };
-    try {
-      const res = await axios.post(url, data);
-      console.log(res);
-      if (res.data.result) {
-        navigate("/calendar");
-      } else {
-        alert("회원가입 실패");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return (
-    <Test>
-      <MainDiv className="MainDiv">
-        <BackColor src={color} style={{ opacity: 0.2 }} />
-        <Usertype>
-          <MdWork /> 기업용
-        </Usertype>
-        <SignupDiv>
-          <form onSubmit={e => handleSubmit(e)}>
-            <Ms style={{ display: `block` }}>필수항목 *</Ms>
-            <Label>
-              이메일을 입력해주세요.
-              <Ms>*</Ms>
-              <Purple>
-                <BsFillCheckCircleFill />
-                이메일은 회원검색으로 사용됩니다.
-              </Purple>
-            </Label>
-            <Input
-              type="text"
-              name="email"
-              onChange={_handleInputChange}
-              required
-            // onChange={e => {
-            //handleOnChange("id", e);
-            />
-            <Button onClick={getAuthNum} type="button">
-              인증번호받기
-            </Button>
-            <Green>
-              {/* <BsFillCheckCircleFill /> */}
-              {/* 사용가능한 아이디입니다! */}
-            </Green>
-            <Label>
-              인증번호를 입력해주세요.<Ms>*</Ms>
-            </Label>
-            <Input
-              disabled={state.codeCheck}
-              type="text"
-              name="authCode"
-              onChange={_handleInputChange}
-              required
-            />
-            <Button onClick={codeCheck} type="button">
-              확인
-            </Button>
-            <Label>
-              비밀번호를 입력해주세요.<Ms>*</Ms>
-            </Label>
-            <Input
-              type="password"
-              name="userPw"
-              onChange={_handleInputChange}
-              required
-            />
-            {/* <span onClick={handlePasswordType}>
-            {pwType.visible ? <AiFillEye /> : <AiFillEyeInvisible />}
-          </span> */}
-            <PwMsg>
-              비밀번호는 영문 대소문자, 특수문자(*,!,~,?)를 포함하여
-              <br />
-              8~16자 사이로 설정해주세요.
-            </PwMsg>
-            <Label>
-              이름을 입력해주세요.<Ms>*</Ms>
-            </Label>
-            <Input
-              type="text"
-              name="name"
-              onChange={_handleInputChange}
-              required
-            />
-            <BtnDiv>
-              {state.emailCheck && (
-                <NextBtn>
-                  <IoIosArrowForward style={{ width: 25, height: 25 }} />
-                </NextBtn>
-              )}
-            </BtnDiv>
-          </form>
-        </SignupDiv>
-      </MainDiv>
-    </Test>
-  );
-};
-export default SignUpCorpPage;
-
+// styles
 const Test = styled.div`
   width: 100vw;
   height: 100vh;
   background: #f5f3fe;
 `;
+
 const MainDiv = styled.div`
   position: absolute;
   width: 90vw;
@@ -197,6 +34,7 @@ const MainDiv = styled.div`
   flex-direction: row;
   z-index: 2;
 `;
+
 const BackColor = styled.img`
   position: absolute;
   width: 548px;
@@ -206,7 +44,8 @@ const BackColor = styled.img`
   background: #f8f8f8;
   z-index: 1;
 `;
-const Usertype = styled.div`
+
+const UserType = styled.div`
   position: absolute;
   margin: 35px 100px;
   color: #8165df;
@@ -216,12 +55,14 @@ const Usertype = styled.div`
   font-size: 20px;
   line-height: 24px;
 `;
+
 const SignupDiv = styled.div`
   margin: 30px auto;
   padding: 30px;
   z-index: 3;
   text-align: left;
 `;
+
 const Ms = styled.span`
   font-style: normal;
   font-weight: 700;
@@ -230,6 +71,7 @@ const Ms = styled.span`
   color: #d63031;
   text-align: left;
 `;
+
 const Label = styled.div`
   color: #404248;
   text-align: left;
@@ -240,6 +82,7 @@ const Label = styled.div`
   font-size: 11px;
   line-height: 13px;
 `;
+
 const Purple = styled.span`
   font-family: "Inter";
   font-style: normal;
@@ -249,6 +92,7 @@ const Purple = styled.span`
   color: #8165df;
   margin-left: 5px;
 `;
+
 const Green = styled.div`
   font-family: "Inter";
   font-style: normal;
@@ -259,6 +103,7 @@ const Green = styled.div`
   text-align: left;
   margin: 5px 10px;
 `;
+
 const Input = styled.input`
   width: 300px;
   height: 20px;
@@ -266,6 +111,7 @@ const Input = styled.input`
   background: #ffffff;
   margin-left: 10px;
 `;
+
 const Button = styled.button`
   height: 22px;
   margin-left: 20px;
@@ -278,6 +124,7 @@ const Button = styled.button`
   line-height: 10px;
   color: #ffffff;
 `;
+
 const PwMsg = styled.div`
   font-style: normal;
   font-weight: 400;
@@ -287,10 +134,12 @@ const PwMsg = styled.div`
   text-align: left;
   margin: 5px 10px;
 `;
+
 const BtnDiv = styled.div`
   text-align: center;
   margin-top: 20px;
 `;
+
 const NextBtn = styled.button`
   width: 40px;
   height: 40px;
@@ -300,3 +149,159 @@ const NextBtn = styled.button`
   background: #8165df;
   box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.25);
 `;
+
+const SignUpCorpPage = () => {
+  const navigate = useNavigate();
+  const [authCode, setAuthCode] = useState(null);
+  const [isAuthCode, setIsAuthCode] = useState(false);
+  const [form, setForm] = useState({
+    email: null,
+    password: null,
+    username: null,
+  });
+
+  const handleInputChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // 인증코드 요청
+  const getAuthNum = async e => {
+    e.preventDefault();
+    // const url = `http://${process.env.REACT_APP_SECRET_URL}/users/email-verify`;
+    // const data = {
+    //   email: signUp.email,
+    // };
+    // try {
+    //   const res = await axios.post(url, data, { withCredentials: true });
+    //   setState({
+    //     ...state,
+    //     codeCheck: false,
+    //   });
+    //   console.log(res);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+
+  // 인증코드 확인
+  const codeCheck = async e => {
+    e.preventDefault();
+    // const url = `http://${process.env.REACT_APP_SERVER_URI}/users/email-authentication`;
+    // const data = {
+    //   authNum: state.authCode,
+    // };
+    // try {
+    //   const res = await axios.post(url, data);
+    //   console.log(res);
+    //   if (res.data.result) {
+    //     setState({
+    //       ...state,
+    //       emailCheck: true,
+    //     });
+    //     alert("인증 성공");
+    //   } else {
+    //     alert("잘못된 인증번호입니다.");
+    //   }
+    // } catch (error) {
+    //   console.log(error);
+    // }
+  };
+
+  // 회원가입
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      email: form.email,
+      password: form.password,
+      username: form.username,
+    };
+
+    try {
+      const res = await fetchOfficeSignUp(data);
+
+      if (res.data) {
+        navigate("/auth/sign-in");
+      } else {
+        alert("회원가입 실패");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Test>
+      <MainDiv className="MainDiv">
+        <BackColor src={color} style={{ opacity: 0.2 }} />
+        <UserType>
+          <MdWork /> 기업용
+        </UserType>
+        <SignupDiv>
+          <form onSubmit={e => handleSubmit(e)}>
+            <Ms style={{ display: `block` }}>필수항목 *</Ms>
+            <Label>
+              이메일을 입력해주세요.
+              <Ms>*</Ms>
+            </Label>
+            <Input
+              type="text"
+              name="email"
+              onChange={handleInputChange}
+              required
+            />
+            <Button onClick={getAuthNum} type="button">
+              인증번호받기
+            </Button>
+            <Label>
+              인증번호를 입력해주세요.<Ms>*</Ms>
+            </Label>
+            <Input
+              disabled={isAuthCode}
+              type="text"
+              name="authCode"
+              onChange={handleInputChange}
+              required
+            />
+            <Button onClick={codeCheck} type="button">
+              확인
+            </Button>
+            <Label>
+              비밀번호를 입력해주세요.<Ms>*</Ms>
+            </Label>
+            <Input
+              type="password"
+              name="password"
+              onChange={handleInputChange}
+              required
+            />
+            <PwMsg>
+              비밀번호는 영문 대소문자, 특수문자(*,!,~,?)를 포함하여
+              <br />
+              8~16자 사이로 설정해주세요.
+            </PwMsg>
+            <Label>
+              이름을 입력해주세요.<Ms>*</Ms>
+            </Label>
+            <Input
+              type="text"
+              name="username"
+              onChange={handleInputChange}
+              required
+            />
+            <BtnDiv>
+              <NextBtn>
+                <IoIosArrowForward style={{ width: 25, height: 25 }} />
+              </NextBtn>
+            </BtnDiv>
+          </form>
+        </SignupDiv>
+      </MainDiv>
+    </Test>
+  );
+};
+
+export default SignUpCorpPage;
