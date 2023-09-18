f5fc263d54032252b308abb38c722d3318eab296
mintmin <mintmin0320@gmail.com>
2023-09-18 21:38:59 +0900

    fix 회원가입 선택 페이지명 변경

--- src/routes/AuthRoute.js
+++ src/routes/AuthRoute.js
@@ -3,14 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import SignInPage from "../pages/auth/SignInPage";
 import PersonalSignUpPage from "../pages/auth/PersonalSignUpPage";
-import SignUpTypePage from "../pages/auth/SignUpTypePage";
+import SignUpPage from "../pages/auth/SignUpPage";
 import SignUpCorpPage from "../pages/auth/SignUpCorpPage";
 
 export default function AuthRoute() {
   return (
     <Routes>
       <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/" element={<SignUpTypePage />} />
+      <Route path="/" element={<SignUpPage />} />
       <Route path="/personal" element={<PersonalSignUpPage />} />
       <Route path="/corp" element={<SignUpCorpPage />} />
     </Routes>
