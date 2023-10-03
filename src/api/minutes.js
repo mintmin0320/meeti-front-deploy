d5da416186bf95099789d81b17f6c56650765ae7
mintmin <mintmin0320@gmail.com>
2023-10-04 00:38:29 +0900

    refactor: BASEURL 제거 및 URL 상수화

--- src/api/minutes.js
+++ src/api/minutes.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
-const BASE_URL = `https://${process.env.REACT_APP_SERVER_URI}`;
+// constants
+import {
+  ADD_MINUTES,
+  DELETE_MINUTES,
+  EDIT_MINUTES,
+  READ_MINUTES
+} from '../constants/urls/minutesUrls';
 
 export const fetchGetMinutes = (userId) => {
   try {
-    const res = axios.get(`${BASE_URL}/meeti/meeting/search/${userId}`);
+    const res = axios.get(`${READ_MINUTES}/${userId}`);
 
     return res;
   } catch (error) {
@@ -14,7 +20,7 @@ export const fetchGetMinutes = (userId) => {
 
 export const fetchAddMinutes = (data, userId) => {
   try {
-    const res = axios.post(`${BASE_URL}/meeti/meeting/reg/${userId}`, data);
+    const res = axios.post(`${ADD_MINUTES}/${userId}`, data);
 
     return res;
   } catch (error) {
@@ -24,7 +30,7 @@ export const fetchAddMinutes = (data, userId) => {
 
 export const fetchDeleteMinutes = (meetingId) => {
   try {
-    const res = axios.delete(`${BASE_URL}/meeti/meeting/delete/${meetingId}`);
+    const res = axios.delete(`${DELETE_MINUTES}/${meetingId}`);
 
     return res;
   } catch (error) {
@@ -34,7 +40,7 @@ export const fetchDeleteMinutes = (meetingId) => {
 
 export const fetchEditMinutes = (data, meetingId, userId) => {
   try {
-    const res = axios.post(`${BASE_URL}/meeti/meeting/update/${meetingId}/${userId}`, data);
+    const res = axios.post(`${EDIT_MINUTES}/${meetingId}/${userId}`, data);
 
     return res;
   } catch (error) {
---
+++ src/constants/urls/minutesUrls.js
@@ -0,0 +1,11 @@
+// 회의록 - 회의록 리스트 조회
+export const READ_MINUTES = `/meeti/meeting/search`;
+
+// 회의록 - 희의록 등록(저장)
+export const ADD_MINUTES = `/meeti/meeting/reg`;
+
+// 회의록 - 회의록 삭제
+export const DELETE_MINUTES = `/meeti/meeting/delete`;
+
+// 회의록 - 회의록 수정
+export const EDIT_MINUTES = `/meeti/meeting/update`;
