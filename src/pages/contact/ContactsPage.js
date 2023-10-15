16c8c4e314bf1d44ee2a76fa4b782a62734a7cdb
mintmin <mintmin0320@gmail.com>
2023-10-15 21:01:47 +0900

    refactor: 즐겨찾기. 전체, 요청 컴포넌트 위치 수정

--- src/pages/contact/ContactsPage.js
+++ src/pages/contact/ContactsPage.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import Header from '../../common/Header';
 import ContactList from "../../components/contact/ContactList";
 import FavoritesList from '../../components/contact/FavoritesList';
 import MainContacts from "../../components/contact/MainContacts";
+import FriendRequest from '../../components/contact/FriendRequest';
 
 // bg-color, global-styles
 import color from "./../../assets/color.png";
@@ -16,7 +17,23 @@ import {
 } from '../../styles/CommonStyles';
 
 // styles
-const Title = styled.p`
+const TextBox = styled.div`
+  display: flex;
+`;
+
+const Tittle = styled.button`
+  height: 30px;
+  font-size: 20px;
+  font-weight: 700;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${props => props.isActive ? '#000' : '#d8d8d8'};
+  border: none;
+  background-color:transparent;
+  cursor: pointer;
+`;
+
+const TittleText = styled.p`
   height: 30px;
   font-size: 20px;
   font-weight: 700;
@@ -31,16 +48,36 @@ const RightSection = styled.section`
 `;
 
 const ContactsPage = () => {
+  const [isStatus, setIsStatus] = useState(false);
+
   return (
     <Container>
       <MainSection className="MainDiv">
         <BackColor src={color} style={{ opacity: 0.2 }} />
         <Header />
         <LeftSection>
-          <Title>연락처</Title>
-          <ContactList />
-          <Title>즐겨찾기</Title>
-          <FavoritesList />
+          <TextBox>
+            <Tittle
+              onClick={() => setIsStatus(true)}
+              isActive={isStatus}
+            >
+              연락처 |
+            </Tittle>
+            &nbsp;
+            <Tittle
+              onClick={() => setIsStatus(false)}
+              isActive={!isStatus}
+            >
+              즐겨찾기
+            </Tittle>
+          </TextBox>
+          {!isStatus ?
+            <ContactList />
+            :
+            <FavoritesList />
+          }
+          <TittleText>요청</TittleText>
+          <FriendRequest />
         </LeftSection>
         <RightSection>
           <MainContacts />
