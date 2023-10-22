397f49072b0bca29ff96170948c0d5626d68b4bb
mintmin <mintmin0320@gmail.com>
2023-10-23 03:59:07 +0900

    style: alert 코드 제거

--- src/common/Modal.js
+++ src/common/Modal.js
@@ -17,7 +17,6 @@ const ModalWrap = styled.div`
 const Modal = ({ friendId }) => {
   const userId = localStorage.getItem("userId");
   const [scheduleList, setScheduleList] = useState([]);
-  alert(friendId)
 
   useEffect(() => {
     getScheduleList();
