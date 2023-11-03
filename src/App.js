3f1aa2d3e25ca9532069e55ee156c56fa03852ce
mintmin <mintmin0320@gmail.com>
2023-11-03 20:36:29 +0900

    feat: react-query 기본 설정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
 import MainRoute from "./routes/MainRoute";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <div className="App">
+    <QueryClientProvider client={queryClient}>
       <Router>
         <MainRoute />
       </Router>
-    </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
