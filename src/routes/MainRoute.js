9f6467c904d4da6c1201ed4c37f78f088ee9ebff
mintmin <mintmin0320@gmail.com>
2023-11-11 00:55:02 +0900

    fix: 접근권한 제한 수정

--- src/routes/MainRoute.js
+++ src/routes/MainRoute.js
@@ -13,36 +13,44 @@ import {
   PublicOnlyRoute
 } from './index'
 
+
 export default function MainRoute() {
   return (
     <Routes>
       <Route
         path="/auth/*"
-        element={<PublicOnlyRoute><AuthRoute /></PublicOnlyRoute>}
+        // element={<PublicOnlyRoute><AuthRoute /></PublicOnlyRoute>}
+        element={<AuthRoute />}
       />
       <Route
         path="/*"
-        element={<PrivateRoute><ScheduleRoute /></PrivateRoute>}
+        // element={<PrivateRoute><ScheduleRoute /></PrivateRoute>}
+        element={<ScheduleRoute />}
       />
       <Route
         path="/contact/*"
-        element={<PrivateRoute><ContactRoute /></PrivateRoute>}
+        // element={<PrivateRoute><ContactRoute /></PrivateRoute>}
+        element={<ContactRoute />}
       />
       <Route
         path="/reservation/*"
-        element={<PrivateRoute><ReservationRoute /></PrivateRoute>}
+        // element={<PrivateRoute><ReservationRoute /></PrivateRoute>}
+        element={<ReservationRoute />}
       />
       <Route
         path="/approval/*"
-        element={<PrivateRoute><ApprovalRoute /></PrivateRoute>}
+        // element={<PrivateRoute><ApprovalRoute /></PrivateRoute>}
+        element={<ApprovalRoute />}
       />
       <Route
         path="/profile/*"
-        element={<PrivateRoute><ProfileRoute /></PrivateRoute>}
+        // element={<PrivateRoute><ProfileRoute /></PrivateRoute>}
+        element={<ProfileRoute />}
       />
       <Route
         path="/minutes/*"
-        element={<PrivateRoute><MinutesRoute /></PrivateRoute>}
+        // element={<PrivateRoute><MinutesRoute /></PrivateRoute>}
+        element={<MinutesRoute />}
       />
     </Routes>
   );
