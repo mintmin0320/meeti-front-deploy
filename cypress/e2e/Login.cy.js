dc1cb7577c43814dddce4db061c0cde062ec5b85
mintmin <mintmin0320@gmail.com>
2024-01-10 22:03:57 +0900

    test: 커스텀 커맨드 적용

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -5,17 +5,9 @@ beforeEach(() => {
 it("회원가입을 클릭하면 회원가입(기업/개인) 페이지로 이동하기", () => {
   cy.findByLabelText("회원가입").click();
 
-  cy.url().should("eq", `${Cypress.env('baseUrl')}/auth`);
+  cy.assertUrl('/auth');
 });
 
 it('로그인에 성공하면 일정관리 페이지(메인)로 이동하고 "일정"이라는 페이지명이 노출된다.', () => {
-  const email = 'hamin0320@naver.com';
-  const password = '1234';
-
-  cy.findByLabelText('ID').type(email);
-  cy.findByLabelText('PW').type(password);
-  cy.findByLabelText("로그인").click();
-
-  cy.url().should("eq", `${Cypress.env('baseUrl')}/`);
-  cy.findByText('일정').should('exist');
+  cy.signin();
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,37 @@
 import '@testing-library/cypress/add-commands'
 
+/**
+ * 커스텀 커맨드
+ */
+
+Cypress.Commands.add('signin', () => {
+  const email = 'hamin0320@naver.com';
+  const password = '1234';
+
+  cy.findByLabelText('ID').type(email);
+  cy.findByLabelText('PW').type(password);
+  cy.findByLabelText("로그인").click();
+
+  cy.url().should("eq", `${Cypress.env('baseUrl')}/`);
+  cy.findByText('일정').should('exist');
+});
+
+Cypress.Commands.add('signout', () => {
+  cy.visit('/profile');
+
+  cy.findByLabelText("로그아웃").click();
+
+  cy.url().should("eq", `${Cypress.env('baseUrl')}/auth/sign-in`);
+});
+
+Cypress.Commands.add('assertUrl', (url) => {
+  cy.url().should("eq", `${Cypress.env('baseUrl')}${url}`);
+});
+
+/**
+ * 커스텀 쿼리
+ */
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
